d8efedd382395654fbd5fa695135cc5a2bbf23ba
Lyndon Panton <LyndonPanton@gmail.com>
2023-03-13 01:00:48 +0000

    Added some start code

---
+++ Exercise8/Assets/Scripts/EventManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+public static class EventManager
+{
+    //Invoker invoker;
+    //Listener listener;
+}
